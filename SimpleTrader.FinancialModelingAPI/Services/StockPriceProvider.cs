fbe0f72e91300d872c6426787c40e40269b7efc2
TIIbragimov <timur.ibragimov1@beeline.kg>
2025-04-20 20:16:09 +0600

    add db context and factory

---
+++ SimpleTrader.FinancialModelingAPI/Services/StockPriceProvider.cs
@@ -0,0 +1,70 @@
+﻿using SimpleTrader.Domain.Exceptions;
+using SimpleTrader.Domain.Services.Interfaces;
+using SimpleTrader.FinancialModelingAPI.Results;
+
+namespace SimpleTrader.FinancialModelingAPI.Services
+{
+    public class StockPriceProvider : IStockPriceService
+    {
+        // URI to get the stock time
+        private const string _stockRealTimeGlobal = "stock/real-time-price/";
+        // FinancialModelingHttpClientFactory to create the HttpClient
+        private readonly FinancialModelingHttpClient _financialModelingHttpClient;
+
+        public StockPriceProvider(FinancialModelingHttpClient financialModelingHttpClient)
+        {
+            _financialModelingHttpClient = financialModelingHttpClient;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidSymbolException"></exception>
+        /// <exception cref="Exception"></exception>
+        public async Task<double> GetPriceAsync(string symbol)
+        {
+            double returnFirstPrice = 0;
+            // uri to get the StockPriceResult according to the symbol
+            string fullUriToMajorIndex = _stockRealTimeGlobal + symbol;
+
+            // Get the response message from the uri
+            StockPriceResult stockResult = await _financialModelingHttpClient.GetAsync<StockPriceResult>(fullUriToMajorIndex);
+
+            if(stockResult == null)
+            {
+                throw new Exception("The stock does not exist");
+            }
+            else
+            {
+                if (stockResult.CompaniesPriceList.Count == 0)
+                {
+                    throw new InvalidSymbolException(symbol);
+                }
+
+                try
+                {
+                    returnFirstPrice = stockResult.CompaniesPriceList
+                        .Where(c => c.Symbol == symbol)
+                        .First().Price;
+                }
+                catch (NullReferenceException)
+                {
+                    throw new Exception("The price of the stock is not available");
+                }
+                catch (InvalidOperationException)
+                {
+                    throw new Exception("The price of the stock is not available");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"{ex.Message}");
+                }
+            }
+
+            // Return the price of stock
+            return returnFirstPrice;
+        }
+    }
+}
