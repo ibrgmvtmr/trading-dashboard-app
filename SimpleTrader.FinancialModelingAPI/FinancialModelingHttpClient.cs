a4401c8d2e9d20487f9eecad214d8d8d3329e863
TIIbragimov <timur.ibragimov1@beeline.kg>
2025-04-20 20:20:10 +0600

    add home view

---
+++ SimpleTrader.FinancialModelingAPI/FinancialModelingHttpClient.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using SimpleTrader.FinancialModelingAPI.Models;
+using System.Net;
+
+namespace SimpleTrader.FinancialModelingAPI
+{
+    public class FinancialModelingHttpClient 
+    {
+        // API key for the financial modeling API
+        private readonly string _apiKey;
+        // HTTP client to make the requests
+        private readonly HttpClient _httpClient;
+
+        public FinancialModelingHttpClient(HttpClient httpClient, FinancialModelingAPIKey apiKey)
+        {
+            _httpClient = httpClient;
+            _apiKey = apiKey.Key;
+        }
+        /// <summary>
+        /// Async base method to get the response message from the uri
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        public async Task<T> GetAsync<T>(string uri)
+        {
+            try
+            {
+                // Get the response message from the uri
+                HttpResponseMessage responseMessage = await _httpClient.GetAsync(uri + $"?apikey={_apiKey}");
+
+                // Get the raw content from the response message
+                string rawContentJson = await responseMessage.Content.ReadAsStringAsync();
+
+                if (string.IsNullOrEmpty(rawContentJson))
+                {
+                    throw new Exception("Empty response from API");
+                }
+
+                if(rawContentJson.Contains("[]"))
+                {
+                    return default;
+                }
+
+                // Deserialize the raw content to T object and return it
+                return JsonConvert.DeserializeObject<T>(rawContentJson);
+            }
+            catch (WebException webEx)
+            {
+               using(var respone = webEx.Response)
+               {
+                    using(var data = respone.GetResponseStream())
+                    {
+                        string text = new StreamReader(data).ReadToEnd();
+                        throw new Exception(text);
+                    }
+               }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                // Handle HTTP specific errors
+                throw new Exception($"Request error: {httpEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to get data from the API: {ex}");
+            }
+        }
+    }
+}
---
+++ SimpleTrader.WPF/VVM/ViewModels/HomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleTrader.WPF.VVM.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public AssetSummaryViewModel AssetSummaryViewModel { get; }
+        public MajorIndexListingViewModel MajorIndexListingViewModel { get;  }
+
+        public HomeViewModel(MajorIndexListingViewModel majorIndexViewModel, AssetSummaryViewModel assetSummaryViewModel)
+        {
+            MajorIndexListingViewModel = majorIndexViewModel;
+            AssetSummaryViewModel = assetSummaryViewModel;
+        }
+
+        public override void Dispose()
+        {
+            AssetSummaryViewModel?.Dispose();
+            MajorIndexListingViewModel?.Dispose();
+
+            base.Dispose();
+        }
+    }
+}
